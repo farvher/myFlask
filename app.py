e44c5a822c0e756b6781d621b20ee915c0bd54c7
fsanmiguel <farith.sanmiguel@middlesoftware.com>
2020-07-05 18:30:40 -0500

    article template

--- app.py
+++ app.py
@@ -1,56 +1,46 @@
 from flask import *
-import functools
-from werkzeug.security import *
+
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///article.sqlite3'
 app.config['SECRET_KEY'] = "random string"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-Model = db.Model
 
+from db import Article,Category
 
-class Students(db.Model):
-    id = db.Column('student_id', db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    city = db.Column(db.String(50))
-    addr = db.Column(db.String(200))
-    pin = db.Column(db.String(10))
-
-
-def __init__(self, name, city, addr, pin):
-    self.name = name
-    self.city = city
-    self.addr = addr
-    self.pin = pin
-
+db.drop_all()
+db.create_all()
+db.session.commit()
 
 @app.route('/')
-def show_all():
-    return render_template('show_all.html', students=Students.query.all())
+def articles():
+    return render_template('articles.html', articles=Article.query.all())
 
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+        if not request.form['title'] or not request.form['body'] or not request.form['author']:
             flash('Please enter all the fields', 'error')
         else:
-            student = Students(id=None,
-                               name=request.form['name'],
-                               city=request.form['city'],
-                               addr=request.form['addr'],
-                               pin=request.form['pin'])
+            article = Article(title=request.form['title'],
+                              sub_title=request.form['subtitle'],
+                              body=request.form['body'],
+                              author=request.form['author'],
+                              lecture_time=5,
+                              category_id=1)
 
-            db.session.add(student)
+            db.session.add(article)
             db.session.commit()
 
             flash('Record was successfully added')
-            return redirect(url_for('show_all'))
+            return redirect(url_for('articles'))
     return render_template('new.html')
 
 
+
 if __name__ == '__main__':
-    """ Crear la bases de datos"""
-    db.create_all()
     app.run()
--- auth.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
-
-bp = Blueprint('auth', __name__, url_prefix='/auth')
